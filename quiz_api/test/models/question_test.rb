c613352a4164575e976fdde64e5945f0958dc286
Bertie Croll <bert.croll@gmail.com>
2017-02-01 10:15:27 +0000

    has_many set up for Quiz and Question

--- quiz_api/test/models/question_test.rb
+++ quiz_api/test/models/question_test.rb
@@ -7,4 +7,7 @@ class QuestionTest < ActiveSupport::TestCase
   test "Question has a quiz" do
     assert questions(:one).quiz = quizzes(:one)
   end
+  test "Question can count answers" do
+   assert_equal(2, questions(:one).answers.count)
+  end
 end
--- quiz_api/test/models/quiz_test.rb
+++ quiz_api/test/models/quiz_test.rb
@@ -4,4 +4,7 @@ class QuizTest < ActiveSupport::TestCase
  test "Quiz has a title" do
    assert_equal("Quiz One", quizzes(:one).title)
  end
+ test "Quiz can count questions" do
+  assert_equal(1, quizzes(:one).questions.count)
+ end
 end

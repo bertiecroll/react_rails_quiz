022bdfd47c5845eca91be5c17c0725112a4f121b
Bertie Croll <bert.croll@gmail.com>
2017-02-01 15:46:06 +0000

    Quiz updated to check if quiz is complete

--- quiz_ui/client/src/components/Quiz.jsx
+++ quiz_ui/client/src/components/Quiz.jsx
@@ -14,19 +14,33 @@ class Quiz extends React.Component {
 
   render() {
     const question = this.props.questions[this.state.currentQuestion]
+    const content = (this.state.complete) ?
+      "Completed!" :
+      <Question question={question} submitAnswer={this.submitAnswer}/>
     return ( 
       <div className="quiz">
-        <Question question={question} submitAnswer={this.submitAnswer}/>
+        {content}  
       </div>
     )
   }
 
   submitAnswer(answerIndex) {    
     this.state.answers[this.state.currentQuestion] = answerIndex
+    const isComplete = this.isQuizComplete()
     this.setState({
       currentQuestion: this.state.currentQuestion + 1,
+      complete: isComplete
     })
     console.log("answers array", this.state.answers)
+    console.log("quiz completed?", this.state.complete)
+  }
+
+  isQuizComplete() {
+    const atEndOfTest = this.state.answers.length === this.props.questions.length
+    const allAnswered = this.state.answers.every(function(answer) {
+      return answer !== undefined
+    })
+    return (atEndOfTest && allAnswered)
   }
 }
 

3825418ee01d05a36bb6322cd229b20d996cd6a5
Bertie Croll <bert.croll@gmail.com>
2017-02-02 11:56:32 +0000

    app.js updated to use redux Provider

--- quiz_ui/client/src/app.js
+++ quiz_ui/client/src/app.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import QuizReducer from './src/reducers/quiz'
 import App from './containers/App'
+
+const store = createStore(QuizReducer)
 
 window.onload = function(){
   ReactDOM.render(
-    <App url="http://localhost:3000/"/>,
+    <Provider store={store}>
+      <App url="http://localhost:3000/"/>
+    </Provider>,
     document.getElementById('app')
   )
 }

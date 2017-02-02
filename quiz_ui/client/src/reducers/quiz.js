5937775da8f6c77e72b0a20a9b0225d3e08424b6
Bertie Croll <bert.croll@gmail.com>
2017-02-02 10:39:45 +0000

    switch case added to quiz reducer

--- quiz_ui/client/src/reducers/quiz.js
+++ quiz_ui/client/src/reducers/quiz.js
@@ -7,3 +7,34 @@ const initialState = {
   scoreCard: [],
   complete: false
 }
+
+const Quiz = function(state=initialState, action) {
+  switch(action.type) {
+    case QuizActionTypes.SET_QUIZ:
+      return Object.assign({}, state, {
+        quiz: action.quiz
+      })
+    case QuizActionTypes.SET_USER:
+      return Object.assign({}, state, {
+        currentUser: action.user
+      })
+    case QuizActionTypes.UPDATE_SCORECARD:
+      return Object.assign({}, state, {
+        scoreCard: state.scoreCard.slice(0, action.index)
+                      .concat([action.score])
+                      .concat(state.scoreCard.slice(action.index + 1))
+      })
+    case QuizActionTypes.UPDATE_QUESTION_INDEX:
+      return Object.assign({}, state, {
+        currentQuestion: action.questionIndex
+      })
+    case QuizActionTypes.TOGGLE_COMPLETE:
+      return Object.assign({}, state, {
+        complete: !state.complete
+      })
+    default:
+      return state
+  }
+}
+
+export default Quiz
